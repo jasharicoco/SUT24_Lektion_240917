7ef3db3d43ab23247863568721ab49859e95ac45
Nomijah <petter.bostrom@gmail.com>
2024-09-17 11:09:57 +0200

    Added a calculation

--- SUT24_Lektion_240917/Program.cs
+++ SUT24_Lektion_240917/Program.cs
@@ -6,6 +6,11 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("I said HELLO!");
+
+            int number = 5;
+            int number2 = 7;
+            int sum = number + number2;
+            Console.WriteLine(sum);
         }
     }
 }
